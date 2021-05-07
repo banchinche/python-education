85670679378826d92225adaa4e78a374370061c7
banchinche <andrew.hrimov@gmail.com>
2021-05-07 11:19:38 +0300

    Changed initializing instances process with arguments to make 10/10 pylint check

--- python/oop/transports.py
+++ python/oop/transports.py
@@ -8,21 +8,18 @@ class Transport:
     """
     Base transport class
     """
-    _number_of_wheels = 0
+    number_of_wheels = 0
 
     def __init__(self,
-                 speed: float,
-                 weight: float,
-                 color: str):
+                 *args):
         """
         Initializing instance of the class
-        :param speed: float, shows speed of the transport
-        :param weight: float, shows weight of the transport
-        :param color: str, color of the transport
+        :param args: Arguments that will be parsed
         """
-        self.speed: float = speed
-        self.weight: float = weight
-        self.color: str = color
+        self.speed: float = float()
+        self.weight: float = float()
+        self.color: str = str()
+        self.parse_arguments(*args)
 
     def show_info(self):
         """
@@ -35,6 +32,17 @@ class Transport:
               f'Color: {self.color}',
               sep='\n')
 
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 3
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+
 
 class Engine:
     """
@@ -45,20 +53,16 @@ class Engine:
                          'Boxer',
                          'W-type')
 
-    def __init(self, power: float,
-               torque: float,
-               consumption: float):
+    def __init__(self, *args):
         """
         Initializing instance of the engine
-        :param power: float, shows power of the engine
-        :param torque: float, shows torque of the engine
-        :param consumption: float, shows specific fuel consumption of the engine
+        :param args: Arguments that will be parsed
         :return:
         """
-        self.power: float = power
-        self.torque: float = torque
-        self.spec_fuel_consumption: float = consumption
+        self.power: float = float()
+        self.torque: float = float()
         self.category = choice(self.possible_category)
+        self.parse_arguments(*args)
 
     def show_info(self):
         """
@@ -68,27 +72,32 @@ class Engine:
         print('Engine has such characteristics:',
               f'- power: {self.power}',
               f'- torque: {self.torque}',
-              f'- specific fuel consumption: {self.spec_fuel_consumption}',
               f'- category: {self.category}',
               sep='\n')
+
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 2
+        self.power = args[0]
+        self.torque = args[1]
 
 
 class Bicycle(Transport):
     """
     Bicycle transport inheritor
     """
-    _number_of_wheels = 2
+    number_of_wheels = 2
     possible_brands = ('TopRider',
                        'Apollo',
                        'Cyclone',
                        'Discovery',
                        'Formula')
 
-    def __init__(self,
-                 speed: float,
-                 weight: float,
-                 color: str,
-                 brand: str):
+    def __init__(self, *args):
         """
         Initializing instance of the bicycle
         :param speed: float, shows speed of the bicycle
@@ -96,11 +105,8 @@ class Bicycle(Transport):
         :param color: str, color of the bicycle
         :param brand: str, brand of the bicycle
         """
-        super().__init__(speed, weight, color)
-        self.brand: str
-        if brand in self.possible_brands:
-            self.brand = brand
-        else:
+        super().__init__(*args)
+        if self.brand not in self.possible_brands:
             self.brand = choice(self.possible_brands)
 
     def show_info(self):
@@ -115,41 +121,23 @@ class Bicycle(Transport):
               f'Brand: {self.brand}',
               sep='\n')
 
+    def parse_arguments(self, *args):
+        assert len(args) == 4
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+        self.brand = args[3]
+
 
 class Motorcycle(Bicycle, Engine):
     """
     Motorcycle bicycle and Engine inheritor
     """
-    _number_of_wheels = 2
+    number_of_wheels = 2
     possible_brands = ('BMW',
-                       'Harley-Davidson',
                        'Honda',
                        'Kawasaki',
                        'Suzuki')
-
-    def __init__(self,
-                 speed: float,
-                 weight: float,
-                 color: str,
-                 brand: str,
-                 power: float,
-                 torque: float,
-                 consumption: float):
-        """
-        Initializing instance of the motorcycle
-        :param speed: float, shows speed of the motorcycle
-        :param weight: float, shows weight of the motorcycle
-        :param color: str, color of the motorcycle
-        :param brand: str, brand of the motorcycle
-        :param power: float, shows power of the engine
-        :param torque: float, shows torque of the engine
-        :param consumption: float, shows specific fuel consumption of the engine
-        """
-        super().__init__(speed, weight, color, brand)
-        self.power = power
-        self.torque = torque
-        self.spec_fuel_consumption = consumption
-        self.category = choice(Engine.possible_category)
 
     def show_info(self):
         """
@@ -164,26 +152,34 @@ class Motorcycle(Bicycle, Engine):
               sep='\n')
         Engine.show_info(self)
 
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 6
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+        self.brand = args[3]
+        self.power = args[4]
+        self.torque = args[5]
+        self.category = choice(self.possible_category)
+
 
 class Car(Transport, Engine):
     """
     Car transport inheritor
     """
-    _number_of_wheels = 4
+    number_of_wheels = 4
     possible_brands = ('Alfa Romeo',
                        'Hyundai',
                        'Reno',
                        'Opel',
                        'Mercedes')
 
-    def __init__(self,
-                 speed: float,
-                 weight: float,
-                 color: str,
-                 brand: str,
-                 power: float,
-                 torque: float,
-                 consumption: float):
+    def __init__(self, *args):
         """
         Initializing instance of the car
         :param speed: float, shows speed of the car
@@ -192,17 +188,10 @@ class Car(Transport, Engine):
         :param brand: str, brand of the car
         :param power: float, shows power of the engine
         :param torque: float, shows torque of the engine
-        :param consumption: float, shows specific fuel consumption of the engine
         """
-        super().__init__(speed, weight, color)
-        if brand in self.possible_brands:
-            self.brand = brand
-        else:
+        super().__init__(*args)
+        if self.brand not in self.possible_brands:
             self.brand = choice(self.possible_brands)
-        self.power = power
-        self.torque = torque
-        self.spec_fuel_consumption = consumption
-        self.category = choice(Engine.possible_category)
 
     def show_info(self):
         """
@@ -217,27 +206,35 @@ class Car(Transport, Engine):
               sep='\n')
         Engine.show_info(self)
 
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 6
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+        self.brand = args[3]
+        self.power = args[4]
+        self.torque = args[5]
+        self.category = choice(self.possible_category)
+
 
 class Plane(Transport, Engine):
     """
     Plane transport inheritor
     """
-    _number_of_wheels = choice([3, 4, 16, 18])
-    _number_of_wings = choice([2, 4])
+    number_of_wheels = choice([3, 4, 16, 18])
+    number_of_wings = choice([2, 4])
     possible_brands = ('Airbus',
                        'Boeing',
                        'Lockheed Martin',
                        'Northrop Grumman',
                        'Rolls-Royce Holdings')
 
-    def __init__(self,
-                 speed: float,
-                 weight: float,
-                 color: str,
-                 brand: str,
-                 power: float,
-                 torque: float,
-                 consumption: float):
+    def __init__(self, *args):
         """
         Initializing instance of the plane
         :param speed: float, shows speed of the plane
@@ -246,17 +243,11 @@ class Plane(Transport, Engine):
         :param brand: str, brand of the plane
         :param power: float, shows power of the engine
         :param torque: float, shows torque of the engine
-        :param consumption: float, shows specific fuel consumption of the engine
         """
-        super().__init__(speed, weight, color)
-        if brand in self.possible_brands:
-            self.brand = brand
-        else:
+
+        super().__init__(*args)
+        if self.brand not in self.possible_brands:
             self.brand = choice(self.possible_brands)
-        self.power = power
-        self.torque = torque
-        self.spec_fuel_consumption = consumption
-        self.category = choice(Engine.possible_category)
 
     def show_info(self):
         """
@@ -268,36 +259,32 @@ class Plane(Transport, Engine):
               f'Weight: {self.weight} kilos',
               f'Color: {self.color}',
               f'Brand: {self.brand}',
-              f'Count of aircraft wings: {self._number_of_wings}',
+              f'Count of aircraft wings: {self.number_of_wings}',
               sep='\n')
         Engine.show_info(self)
+
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 6
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+        self.brand = args[3]
+        self.power = args[4]
+        self.torque = args[5]
+        self.category = choice(self.possible_category)
 
 
 class JetSki(Motorcycle):
     """
     Jet ski  transport and motorcycle inheritor
     """
-    number_of_wheels = Transport._number_of_wheels
+    number_of_wheels = Transport.number_of_wheels
     possible_brands = Motorcycle.possible_brands
-
-    def __init__(self, speed: float,
-                 weight: float,
-                 color: str,
-                 brand: str,
-                 power: float,
-                 torque: float,
-                 consumption: float):
-        """
-        Initializing instance of the jet ski
-        :param speed: float, shows speed of the jet ski
-        :param weight: float, shows weight of the jet ski
-        :param color: str, color of the jet ski
-        :param brand: str, brand of the jet ski
-        :param power: float, shows power of the engine
-        :param torque: float, shows torque of the engine
-        :param consumption: float, shows specific fuel consumption of the engine
-        """
-        super().__init__(speed, weight, color, brand, power, torque, consumption)
 
     def show_info(self):
         """
@@ -312,24 +299,39 @@ class JetSki(Motorcycle):
               sep='\n')
         Engine.show_info(self)
 
+    def parse_arguments(self, *args):
+        """
+        Parsing arguments to avoid pylint mistakes with too much arguments
+        :param args:
+        :return:
+        """
+        assert len(args) == 6
+        self.speed = args[0]
+        self.weight = args[1]
+        self.color = args[2]
+        self.brand = args[3]
+        self.power = args[4]
+        self.torque = args[5]
+        self.category = choice(self.possible_category)
+
 
 if __name__ == '__main__':
     something1 = Transport(45, 23.5, 'red')
     something1.show_info()
     print()
-    something2 = Bicycle(20, 15.7, 'blue', 'adspf')
+    something2 = Bicycle(20, 15.7, 'blue', 'asap')
     something2.show_info()
     print()
-    something3 = Motorcycle(320, 82.5, 'black', 'seqweqw', 120.5, 150.2, 102.5)
+    something3 = Motorcycle(320, 82.5, 'black', 'seqweqw', 120.5, 150.2)
     something3.show_info()
     print()
-    print(Motorcycle.__mro__)
-    something4 = Car(290, 24.13584e3, 'white', 'seqweqw', 120.5, 150.2, 102.5)
+    # print(Motorcycle.__mro__)
+    something4 = Car(290, 24.13584e3, 'white', 'seqweqw', 120.5, 150.2)
     something4.show_info()
     print()
-    something5 = Plane(600, 7.21451e3, 'azure', 'sasdkFJAJF', 120.5, 150.2, 102.5)
+    something5 = Plane(600, 7.21451e3, 'azure', 'sasdkFJAJF', 120.5, 150.2)
     something5.show_info()
     print()
-    something6 = JetSki(320, 72.5, 'yellow', 'sewqeqwe', 120.5, 150.2, 102.5)
+    something6 = JetSki(320, 72.5, 'yellow', 'sewqeqwe', 120.5, 150.2)
     something6.show_info()
-    print(JetSki.__mro__)
+    # print(JetSki.__mro__)
