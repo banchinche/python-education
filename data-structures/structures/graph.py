b76ddd00c6b6efa0491a1728a8cf57c35971d6c8
banchinche <andrew.hrimov@gmail.com>
2021-05-31 22:53:05 +0300

    Added graph realization using linked list (nodes with links)

--- data-structures/structures/graph.py
+++ data-structures/structures/graph.py
@@ -2,6 +2,7 @@
 Graph realization module
 """
 from typing import Union
+from linked_list import LinkedList
 
 
 class Graph:
@@ -131,6 +132,99 @@ class Graph:
             print(n, end=' ')
 
 
+class Node(LinkedList.Node):
+    """
+    Inheritor of the Node in linked list
+    """
+    def __init__(self, value=None, next_value=None) -> None:
+        """
+        Initializes single node
+        :param value: value in node
+        :param next_value: next node
+        """
+        super().__init__(value, next_value)
+
+    def __str__(self) -> str:
+        """
+        Str magic method for showing all chain of nodes in certain edge
+        :return: str
+        """
+        return f'{self.value if self.value else ""} {self.next_value if self.next_value else ""}'
+
+
+class GraphLinked:
+    """
+    Graph realization using linked list (nodes)
+    """
+    def __init__(self, edges: int) -> None:
+        """
+        Initializes graph
+        :param edges: number of the edges in graph
+        """
+        self.edges = edges
+        self.graph = [None] * self.edges
+
+    def add_edge(self, source: int, destination: int) -> None:
+        """
+        Adds edge to graph
+        :param source: source edge
+        :param destination: destination edge
+        :return: None
+        """
+        node = Node(destination)
+        node.next_value = self.graph[source]
+        self.graph[source] = node
+        node = Node(source)
+        node.next_value = self.graph[destination]
+        self.graph[destination] = node
+
+    def print_graph(self) -> None:
+        """
+        Prints every edge in graph and connections
+        :return: None
+        """
+        for i in range(self.edges):
+            temp = self.graph[i]
+            if temp:
+                print("Adjacency list of vertex {}\n".format(i), end="")
+                temp_str = ''
+                try:
+                    while temp:
+                        if temp.value is not None:
+                            temp_str += f'{temp.value} >- '
+                        temp = temp.next_value
+                    temp_str = temp_str[:-3][::-1]
+                    print(f'{i} ->{temp_str}')
+                    print()
+                except AttributeError:
+                    pass
+
+    def delete_node(self, value: int) -> None:
+        """
+        Deletes node from the graph and connections
+        :param value: int
+        :return: str or None / Node
+        """
+        self.graph[value] = None
+        for node in self.graph:
+            while node:
+                if node.value == value and node.next_value is None:
+                    node.value = None
+                node = node.next_value
+
+    def lookup(self, value: int) -> Union[None, Node, str]:
+        """
+        Finds graph edge with certain value
+        :param value: value to find, int
+        :return: node or str message
+        """
+        try:
+            if self.graph is not None:
+                return self.graph[value]
+        except IndexError:
+            return 'There is no such edge in graph!'
+
+
 if __name__ == '__main__':
     g = Graph()
     g.add_edge('1', '2')
@@ -144,3 +238,24 @@ if __name__ == '__main__':
     g.depth_first_search('1')
     print()
     g.breadth_first_search('1')
+
+    print('Graph implementation using linked list...')
+    vertices = 5
+    graph = GraphLinked(vertices)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 4)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)
+
+    graph.print_graph()
+
+    graph.delete_node(0)
+    graph.print_graph()
+
+    f = graph.lookup(1)
+    d = graph.lookup(2)
+    print(f)
+    print(d)
